b27dc0c8815ff1305d7f19dff0eadc0d7bed124f
aubrey <mantiriaubrey@apps.ipb.ac.id>
2024-05-30 21:45:34 +0700

    next.js-rkpl

--- src/app/_components/MenuComponent.tsx
+++ src/app/_components/MenuComponent.tsx
@@ -40,7 +40,7 @@ export default async function MenuComponent() {
                     <div className={caladea.className}><h2 className="text-4xl">{category.title}</h2></div>
                     <div className='mt-4 w-[5ch] h-[2px]  bg-gray-400'></div>
                     <div className="flex justify-center">
-                        <div className="flex flex-wrap justify-center 2xl:justify-start my-6 gap-7">
+                        <div className="flex flex-wrap justify-center 2xl:justify-start my-3 gap-7">
                             {category.menus.map((menu: Menu, index: number) => (
                                 <div key={index}>
                                     <img src={menu.imageUrl} alt={menu.title} className="w-[410px] h-[270px] rounded-lg object-cover "/>
