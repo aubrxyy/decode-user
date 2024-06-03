7bfaea222e9fdf7404ad8b31b9a7cd9639f84e5f
aubrey <mantiriaubrey@apps.ipb.ac.id>
2024-06-03 14:50:43 +0700

    btn fix

--- src/app/_components/PesanComponent.tsx
+++ src/app/_components/PesanComponent.tsx
@@ -15,10 +15,12 @@ export default function PesanComponent() {
         <div className="my-20 mx-auto text-center">
             <div className={caladeaBold.className}><h2 className="text-5xl max-md:text-4xl mb-4">Pesan di Sini!</h2></div>
             <div className={caladea.className}><p className="text-3xl max-md:text-2xl max-md:mx-8 mb-8">Ingin pesan? Klik tombol pesan di bawah ini!</p></div>
-            <div
-            className="text-2xl mt-8 text-nowrap bg-dgreen border-dgreen border-[2px] rounded active:shadow-lg hover:bg-[#f5f5f5] hover:text-dgreen hover:border-[2px] hover:border-dgreen hover:cursor-pointer text-white transition-all lg:mt-12 py-2 w-36 mx-auto">
-                <a href="https://wa.me/6281385976170" target="_blank" rel="noopener noreferrer" title="WhatsApp">Pesan</a>
-            </div>
+            
+            <a href="https://wa.me/6281385976170" target="_blank" rel="noopener noreferrer" title="WhatsApp">
+                <div className="text-2xl mt-8 text-nowrap bg-dgreen border-dgreen border-[2px] rounded active:shadow-lg hover:bg-[#f5f5f5] hover:text-dgreen hover:border-[2px] hover:border-dgreen hover:cursor-pointer text-white transition-all lg:mt-12 py-2 w-36 mx-auto">
+                    Pesan
+                </div>
+            </a>
             
         </div>
     )
